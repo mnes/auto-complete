8350f2377b3f9ece97b3dc1de1398ec18c168c52
mmorito <morito@mnes.org>
2021-03-30 18:04:50 +0900

    Add dist for release

--- projects/auto-complete/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of auto-complete
- */
-
-export * from './lib/auto-complete.service';
-export * from './lib/auto-complete.component';
-export * from './lib/auto-complete.directive';
-export * from './lib/auto-complete.module';
